7a9b39ec03c08ad70b873ef760ec44b9cc2a2990
zpwparsons <peterp@zero2one.co.za>
2023-08-10 10:48:55 +0200

    feat: add empty states to the uses and articles pages

--- resources/views/livewire/pages/articles/list-articles.blade.php
+++ resources/views/livewire/pages/articles/list-articles.blade.php
@@ -1,33 +1,52 @@
 <div>
-    <div class="max-w-2xl w-full mx-auto flex-1">
-        <h1 class="font-bold text-3xl text-slate-900 dark:text-white">
-            Articles
-        </h1>
+    @if($articles->count())
+        <div class="max-w-2xl w-full mx-auto flex-1">
+            <h1 class="font-bold text-3xl text-slate-900 dark:text-white">
+                Articles
+            </h1>
 
-        <hr class="my-8 dark:border-slate-700">
+            <hr class="my-8 dark:border-slate-700">
 
-        <div class="space-y-12">
-            @foreach($articles as $article)
-                <article>
-                    <h2 class="font-bold text-xl">
-                        <x-app.link href="{{ route('articles:show', $article) }}" class="text-blue-600 dark:text-lime-400 hover:underline">
-                            {{ $article->title }}
-                        </x-app.link>
-                    </h2>
+            <div class="space-y-12">
+                @foreach($articles as $article)
+                    <article>
+                        <h2 class="font-bold text-xl">
+                            <x-app.link href="{{ route('articles:show', $article) }}" class="text-blue-600 dark:text-lime-400 hover:underline">
+                                {{ $article->title }}
+                            </x-app.link>
+                        </h2>
 
-                    <p class="my-3">
-                        {{ $article->excerpt }}
-                    </p>
+                        <p class="my-3">
+                            {{ $article->excerpt }}
+                        </p>
 
-                    <div class="flex items-center gap-x-3 mt-2">
-                        <time datetime="2023-06-14 00:00:00" class="text-slate-400 dark:text-slate-500 text-sm block">
-                            {{ $article->formatted_created_at }}
-                        </time>
-                    </div>
-                </article>
-            @endforeach
+                        <div class="flex items-center gap-x-3 mt-2">
+                            <time datetime="2023-06-14 00:00:00" class="text-slate-400 dark:text-slate-500 text-sm block">
+                                {{ $article->formatted_created_at }}
+                            </time>
+                        </div>
+                    </article>
+                @endforeach
 
-            {{ $articles->links('components.app.pagination') }}
+                {{ $articles->links('components.app.pagination') }}
+            </div>
         </div>
-    </div>
+    @else
+        <div>
+            <h1 class="text-3xl font-bold text-center text-slate-900 dark:text-white">
+                Articles Archive Awaits its Stories!
+            </h1>
+
+            <p class="mt-12">
+                It seems we've arrived in an area yet to be filled with tales and knowledge. This shelf is ready to
+                host a collection of insights, experiences, and wisdom that I'm eager to share. Stay tuned for an
+                assortment of narratives, advice, and discoveries that narrate my digital expeditions.
+            </p>
+
+            <p class="mt-8">
+                Craving inspiration? Delve into my <x-app.link href="{{ route('uses') }}" class="text-blue-600 dark:text-lime-400">tools and resources</x-app.link>
+                or a peek into my digital toolbox.
+            </p>
+        </div>
+    @endif
 </div>
--- resources/views/livewire/pages/uses.blade.php
+++ resources/views/livewire/pages/uses.blade.php
@@ -1,7 +1,28 @@
-<x-app.prose>
-    @foreach($tools as $tool)
-        <h2>{{ $tool->category }}</h2>
+<div>
+    @if($tools->count())
+        <x-app.prose>
+            @foreach($tools as $tool)
+                <h2>{{ $tool->category }}</h2>
 
-        {!! $tool->formatted_description !!}
-    @endforeach
-</x-app.prose>
+                {!! $tool->formatted_description !!}
+            @endforeach
+        </x-app.prose>
+    @else
+        <div>
+            <h1 class="text-3xl font-bold text-center text-slate-900 dark:text-white">
+                Toolbox Awaits its Treasures!
+            </h1>
+
+            <p class="mt-12">
+                Looks like we've ventured into uncharted territory! This toolshed is waiting to
+                be stocked with digital wonders that fuel my creations. Check back soon for an
+                array of enchantments, gadgets, and resources that power my digital escapades.
+            </p>
+
+            <p class="mt-8">
+                Feeling curious? Explore my <x-app.link href="{{ route('articles:list') }}" class="text-blue-600 dark:text-lime-400">articles</x-app.link>
+                for insights and tips from my digital journey.
+            </p>
+        </div>
+    @endif
+</div>
