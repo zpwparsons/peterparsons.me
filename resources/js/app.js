3e690fffe07f845214d95d767185189409d26f31
zpwparsons <peterp@zero2one.co.za>
2023-08-22 07:41:18 +0200

    fix: temporarily fix the keyboard input for wire:navigate

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,12 @@ import { toDarkMode, toLightMode, toSystemMode } from './components/theme';
 window.toDarkMode = toDarkMode;
 window.toLightMode = toLightMode;
 window.toSystemMode = toSystemMode;
+
+// Temporary solution for Livewire's "wire:navigate" keyboard input
+document.addEventListener('keydown', (e) => {
+    if (!e.target.hasAttribute('wire:navigate')) return;
+
+    if (e.key.toLowerCase() === 'enter') {
+        Alpine.navigate(e.target.href);
+    }
+});
